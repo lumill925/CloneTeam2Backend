b1c3fac7c71f8036599533e9f7673095089ed555
Jungmin-Noh <lumill925@gmail.com>
2022-08-24 14:29:41 +0900

    [#33] feat: param으로 카테고리별 조회 기능 추가

--- src/main/java/com/sparta/cloneteam2backend/repository/PostRepository.java
+++ src/main/java/com/sparta/cloneteam2backend/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.cloneteam2backend.repository;
 
+import com.sparta.cloneteam2backend.model.Category;
 import com.sparta.cloneteam2backend.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     List<Post> findAllByOrderByCreatedAtDesc();
+
+    List<Post> findAllByPostCategory(Category postCategory);
 }
